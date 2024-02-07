549a8b3cec499709d70a3e4824b78e74f903a6fb
Yotsu61 <154337123+Yotsu61@users.noreply.github.com>
2024-02-07 17:27:29 +0900

    ファビコン修正

--- 42ch.php
+++ 42ch.php
@@ -80,7 +80,7 @@ function h($str)
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel=”icon” href=“/favicon.ico”>
+    <link rel=”icon” href=“favicon.ico”>
 
     <title>42ch</title>
 
